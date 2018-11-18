d6c73ee3e9567806449a465ad80cc48163874034
Mateusz Bryla <brylamat@gmail.com>
2018-11-18 02:28:31 +0100

    done with here api

--- src/api.js
+++ src/api.js
@@ -9,26 +9,51 @@
   
   const geocoder = platform.getGeocodingService();
   const router = platform.getRoutingService();
+  const explore = new H.places.Explore(platform.getPlacesService());
 
   api.geocode = searchText => new Promise((resolve, reject) => {
     geocoder.geocode({
       searchText,
     }, result => {
       const locations = result.Response.View[0].Result;
-      resolve(locations.map(location => {
+      const positions = locations.map(location => {
         const position = location.Location.DisplayPosition;
         return {
           lat: position.Latitude,
           lng: position.Longitude,
         };
-      }));
+      });
+      resolve(positions[0]);
     }, error => {
       reject(error);
     });
   });
 
-  api.route = (start, destination) => new Promise((resolve, reject) => {
-    router.calculateRoute
+  api.route = (startPosition, endPosition) => new Promise((resolve, reject) => {
+    console.log(`routing from [${startPosition.lat}, ${startPosition.lng}] to [${endPosition.lat}, ${endPosition.lng}]`);
+    router.calculateRoute({
+      mode: 'fastest;pedestrian',
+      waypoint0: `geo!${startPosition.lat},${startPosition.lng}`,
+      waypoint1: `geo!${endPosition.lat},${endPosition.lng}`,
+      representation: 'display',
+    }, result => {
+      resolve(result);
+    }, error => {
+      reject(error);
+    });
+  });
+
+  api.pois = (position, radius, categories) => new Promise((resolve, reject) => {
+    explore.request({
+      in: `${position.lat},${position.lng};r=${radius}`,
+      cat: categories,
+    }, {
+      // intentionally empty 
+    }, result => {
+      resolve(result);
+    }, error => {
+      reject(error);
+    })
   });
 
 })();
