c64595fab354d393c33575e2146fb278d3286238
mz8i <maciej.ziarkowski@gmail.com>
2018-11-18 02:55:12 +0100

    Add basic full-screen map

--- src/setupMap.js
+++ src/setupMap.js
@@ -1,7 +1,7 @@
 (function() {
     let map = window.map =  new H.Map(
         document.getElementById('map'),
-        platform.createDefaultLayers().normal.map,
+        here.platform.createDefaultLayers().normal.map,
         {
             center: { lat: 50.05136, lng: 19.944761 },
             zoom: 15
@@ -9,5 +9,5 @@
 
     let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
-    let ui = window.ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
+    let ui = window.ui = H.ui.UI.createDefault(map, here.platform.createDefaultLayers());
 })();
